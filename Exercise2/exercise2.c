ff4db08dc1b495651f331e1636207e68aefb3bea
ARAgasul <55189070+ARAgasul@users.noreply.github.com>
2019-09-11 14:23:54 +0200

    version1

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,10 @@
-/* Here, you should entry the required libraries */
-
-void main(){
-	/* Here, you should write the source code of your program */
+#include<stdio.h>
+main(){
+	int a,b,sum;
+	printf("Introduce number a:");
+	scanf("%d",&a);
+	printf("Introduce nubmer b:");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("The sum is %d",sum);
 }
